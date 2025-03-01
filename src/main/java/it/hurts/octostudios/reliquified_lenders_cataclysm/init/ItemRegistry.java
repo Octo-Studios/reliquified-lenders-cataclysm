b91e3ba11ec46a5820f139718cbfbdc0c23d66c2
Modekh <modekh415@gmail.com>
2025-03-01 12:57:49 +0300

    Started Vacuum Glove

--- src/main/java/it/hurts/octostudios/reliquified_lenders_cataclysm/init/ItemRegistry.java
+++ src/main/java/it/hurts/octostudios/reliquified_lenders_cataclysm/init/ItemRegistry.java
@@ -2,6 +2,8 @@ package it.hurts.octostudios.reliquified_lenders_cataclysm.init;
 
 import it.hurts.octostudios.reliquified_lenders_cataclysm.ReliquifiedLendersCataclysm;
 import it.hurts.octostudios.reliquified_lenders_cataclysm.items.relics.charm.ScouringEyeItem;
+import it.hurts.octostudios.reliquified_lenders_cataclysm.items.relics.charm.VacuumGloveItem;
+import it.hurts.octostudios.reliquified_lenders_cataclysm.items.relics.charm.VoidVortexInBottleItem;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
@@ -16,6 +18,8 @@ public class ItemRegistry {
             ITEMS.register("scouring_eye", ScouringEyeItem::new);
 //    public static final DeferredHolder<Item, VoidVortexInBottleItem> VOID_VORTEX_IN_BOTTLE =
 //            ITEMS.register("void_vortex_in_bottle", VoidVortexInBottleItem::new);
+    public static final DeferredHolder<Item, VacuumGloveItem> VACUUM_GLOVE =
+            ITEMS.register("vacuum_glove", VacuumGloveItem::new);
 
     public static void register(IEventBus bus) {
         ITEMS.register(bus);
