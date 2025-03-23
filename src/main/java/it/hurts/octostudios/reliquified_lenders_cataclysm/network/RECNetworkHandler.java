a153fa5072460b3f8238b8b6dd108739f2ad06d2
Modekh <modekh415@gmail.com>
2025-03-24 00:52:57 +0300

    Deleted useless code from Network Handler

--- src/main/java/it/hurts/octostudios/reliquified_lenders_cataclysm/network/RECNetworkHandler.java
+++ src/main/java/it/hurts/octostudios/reliquified_lenders_cataclysm/network/RECNetworkHandler.java
@@ -1,6 +1,5 @@
 package it.hurts.octostudios.reliquified_lenders_cataclysm.network;
 
-import it.hurts.octostudios.reliquified_lenders_cataclysm.network.packets.GlowingEffectPacket;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
@@ -12,8 +11,5 @@ public class RECNetworkHandler {
     @SubscribeEvent
     public static void register(final RegisterPayloadHandlersEvent event) {
         final PayloadRegistrar registrar = event.registrar("1").executesOn(HandlerThread.NETWORK);
-
-        registrar.playToClient(GlowingEffectPacket.TYPE,
-                GlowingEffectPacket.STREAM_CODEC, GlowingEffectPacket::handle);
     }
 }
